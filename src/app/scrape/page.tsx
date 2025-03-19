20e88dc9b7977b4e5d8763b454dd4aacb10ba950
Shawn Chee <aykaychee2@gmail.com>
2025-03-19 10:16:38 +0800

    Placeholder website for company check

---
+++ src/app/scrape/page.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
+import { IResolvedValues } from "@/utils/ssl-cert/SSLChecker";
+import CompanyCheck from "@/components/companyCheck";
+
+export default function TestScrapePage() {
+  return (
+<div>
+      <CompanyCheck />
+        </div>
+      );
+        
+
+}
+
