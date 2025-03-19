b0699e7fc2b4255cbd9afcd5548747b118d4392d
Shawn Chee <aykaychee2@gmail.com>
2025-03-19 09:59:05 +0800

    (Hooks): API Calls for registration checking function

---
+++ src/hooks/companyCheckHooks/useRegistrationCheck.ts
@@ -0,0 +1,42 @@
+import { useState } from "react";
+
+export function useRegistrationCheck() {
+  const [registrationDetails, setRegistrationDetails] = useState<any | null>(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+  const resetRegistrationCheck = () => {
+    setRegistrationDetails(null);
+    setError(null)
+  }
+
+  const checkRegistration = async (url: string): Promise<any | null> => {
+    if (!url) return null;
+    setLoading(true);
+    setError(null);
+
+    try {
+      const response = await fetch("/api/registrationcheck", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ url }),
+      });
+
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
+      const result = await response.json();
+      setRegistrationDetails(result);
+      return result;
+    } catch (err: any) {
+      console.error("Error checking registration:", err);
+      setError("Error checking registration");
+      return null;
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return { registrationDetails ,loading, error, checkRegistration, resetRegistrationCheck };
+}
