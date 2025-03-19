e47184659e6e5f7d3fde787e92ef037ca53b0020
Shawn Chee <aykaychee2@gmail.com>
2025-03-19 10:15:17 +0800

    (Hooks): API Calls for SSL-Certificate checking

---
+++ src/hooks/companyCheckHooks/useSSLCheck.ts
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import type { IResolvedValues } from "@/utils/ssl-cert/SSLChecker";
+
+export function useSSLCheck() {
+  const [sslDetails, setSSLDetails] = useState<IResolvedValues | null>(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+  const resetSSLCheck = () => {
+    setSSLDetails(null);
+    setError(null)
+  }
+
+  const checkSSL = async (url: string): Promise<IResolvedValues | null> => {
+    if (!url) return null;
+    setLoading(true);
+    setError(null);
+
+    try {
+      const response = await fetch("/api/sslcheck", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ url }),
+      });
+
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      
+      const result = await response.json();
+      console.log("SSL Result:", result);
+      setSSLDetails(result);
+      return result;
+    } catch (err: any) {
+      console.error("Error checking SSL:", err);
+      setError("Error checking SSL certificate");
+      return null;
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return { sslDetails, loading, error, checkSSL,resetSSLCheck };
+}
