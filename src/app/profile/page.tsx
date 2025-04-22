9efe7db0e767035add468f16130d7e8e2ac6cb85
Shawn Chee <aykaychee2@gmail.com>
2025-04-22 23:53:46 +0800

    (Profile): Recent Donations fetched

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,43 +1,9 @@
-"use client";
-import { useEffect, useState } from "react";
-import { useWallet } from "@solana/wallet-adapter-react";
-import { Connection, LAMPORTS_PER_SOL, clusterApiUrl } from "@solana/web3.js";
+import WalletTransaction from "@/components/profile/walletTransaction";
 
-export default function SolanaBalancePage() {
-    const [balance, setBalance] = useState<number | null>(null);
-    const { publicKey, connected } = useWallet();
-    
-    const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
-
-    useEffect(() => {
-        const fetchBalance = async () => {
-            if (publicKey) {
-                try {
-                    const balanceLamports = await connection.getBalance(publicKey);
-                    setBalance(balanceLamports / LAMPORTS_PER_SOL); // Convert lamports to SOL
-                } catch (error) {
-                    console.error("Error fetching balance:", error);
-                    setBalance(null);
-                }
-            }
-        };
-
-        if (connected) {
-            fetchBalance();
-        }
-    }, [publicKey, connected]);
-
+export default function profilePage(){
     return (
-        <div className="p-8 max-w-5xl pt-24 pb-8 min-h-screen mx-auto text-center">
-            <h1 className="text-2xl font-bold mb-4">Phantom Wallet Balance</h1>
-            {connected && publicKey ? (
-                <div className="p-4 border rounded-lg shadow-lg">
-                    <p><strong>Address:</strong> {publicKey.toBase58()}</p>
-                    <p><strong>Balance:</strong> {balance !== null ? `${balance.toFixed(4)} SOL` : "Loading..."}</p>
-                </div>
-            ) : (
-                <p className="text-red-500">Please connect your Phantom wallet.</p>
-            )}
+        <div className="min-h-screen pt-24">
+        <WalletTransaction/>
         </div>
-    );
+    )
 }
