523435e66246e6dfb32cad1e7b77af7fa2c755e7
Shawn Chee <aykaychee2@gmail.com>
2025-04-23 00:26:15 +0800

    Testing pages on profile page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,9 +1,13 @@
+import Leaderboard from "@/components/leaderboard";
+import MilestoneTrackingPersonal from "@/components/profile/milestone-tracking-personal";
 import WalletTransaction from "@/components/profile/walletTransaction";
 
 export default function profilePage(){
     return (
         <div className="min-h-screen pt-24">
+        <MilestoneTrackingPersonal/>
         <WalletTransaction/>
+        <Leaderboard/>
         </div>
     )
 }
